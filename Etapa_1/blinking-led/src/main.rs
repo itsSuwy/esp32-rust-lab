f3c90b4da9c2693c1cdfa470472a144dbe9eb936
Kaori <diego.velazquez.2006@outlook.es>
2026-07-21 17:24:10 -0600

    chore: reorganize practices into phase directories

---
+++ Etapa_1/blinking-led/src/main.rs
@@ -0,0 +1,18 @@
+use esp_idf_svc::hal::{peripherals::Peripherals, gpio::PinDriver, delay::FreeRtos};
+fn main() {
+    esp_idf_svc::sys::link_patches();
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut pin = PinDriver::output(peripherals.pins.gpio5).unwrap();
+
+    loop {
+        pin.set_high().unwrap();
+        log::info!("LED encendido");
+        FreeRtos::delay_ms(500);
+
+        pin.set_low().unwrap();
+        log::info!("LED apagado");
+        FreeRtos::delay_ms(500);
+    }
+}

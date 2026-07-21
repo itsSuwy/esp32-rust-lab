94b68727a05e73c68aa87baa9bad57afc4c696c9
Kaori <diego.velazquez.2006@outlook.es>
2026-07-21 03:08:38 -0600

    refactor(main): remove unnecessary comments and clean up code

--- blinking-led/src/main.rs
+++ blinking-led/src/main.rs
@@ -1,9 +1,6 @@
 use esp_idf_svc::hal::{peripherals::Peripherals, gpio::PinDriver, delay::FreeRtos};
 fn main() {
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
-    // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = Peripherals::take().unwrap();

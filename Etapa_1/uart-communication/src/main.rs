778c59aeb8dbe2c3e59cded5d3b43fe768d11a80
Kaori <diego.velazquez.2006@outlook.es>
2026-07-22 14:45:56 -0600

    feat(uart-communication): UART communication (write only, read pending)

--- Etapa_1/uart-communication/src/main.rs
+++ Etapa_1/uart-communication/src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     esp_idf_svc::sys::link_patches();
     let peripherals = Peripherals::take().unwrap();
     let pins = peripherals.pins; // Se guarda en una variable inmutable la configuracion de perififericos
-    let config = Config::default().baudrate(Hertz(115_200)); // Crea un struct de configuracion modificando su frecuencia interna a 115_220
+    let config = Config::default().baudrate(Hertz(115_200)); // Crea un struct de configuracion modificando su baudrate a 115_20
+    // Fase 1: Enviar informacion
     let mut uart: UartDriver = UartDriver::new(
         peripherals.uart1, // Se reclama el periferico UART1
         pins.gpio1, // Se le asigna funcionamiento de TX
@@ -18,8 +19,12 @@ fn main() {
         Option::<AnyIOPin>::None, // Sin  CTS
         &config, // Se aplican los parametros asignados en la variable config
     ).unwrap();
-    let x = 9;
-    write!(uart, "Hello!\n").unwrap(); // Forma uno para escribir en UART
-    write!(uart, "Value of x is: {x}\n").unwrap();
-    writeln!(uart, "Another way to print!").unwrap(); // Forma dos para escribir en UART
+    let x = 29;
+    uart.write(b"hola\n").unwrap(); // Forma uno para escribir en UART
+    uart.write(b"{x}\n").unwrap(); // No imprime la variable x
+    write!(uart, "Hello!\n").unwrap(); // Forma dos para escribir en UART
+    write!(uart, "Value of x is: {x}\n").unwrap(); // Si imprime la variable x
+    writeln!(uart, "Another way to print!").unwrap(); // Forma tres para escribir en UART
+    // Fase 2: Recibir informacion TODO
+    // Fase 3: Implementar RTS y CTS
 }

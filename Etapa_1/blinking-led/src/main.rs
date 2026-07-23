5510dc88bbbe70afbcaec71b9671c66a6af201e4
Kaori <diego.velazquez.2006@outlook.es>
2026-07-23 00:48:19 -0600

    refactor(blinking-led): improve readability, add logging for consistency

--- Etapa_1/blinking-led/src/main.rs
+++ Etapa_1/blinking-led/src/main.rs
@@ -1,18 +1,21 @@
-use esp_idf_svc::hal::{peripherals::Peripherals, gpio::PinDriver, delay::FreeRtos};
+// Modulos y funciones utilizadas
+use esp_idf_svc::hal::peripherals::Peripherals;
+use esp_idf_svc::hal::gpio::PinDriver;
+use esp_idf_svc::hal::delay::FreeRtos;
 fn main() {
-    esp_idf_svc::sys::link_patches();
-    esp_idf_svc::log::EspLogger::initialize_default();
-
-    let peripherals = Peripherals::take().unwrap();
-    let mut pin = PinDriver::output(peripherals.pins.gpio5).unwrap();
+    esp_idf_svc::sys::link_patches(); // Se aplica el parche de C-SDK
+    esp_idf_svc::log::EspLogger::initialize_default(); // Se inicializa el entorno para usar logs
+    let peripherals = Peripherals::take().unwrap(); // Se toma el control de los perifericos del microcontrolador
+    let pins = peripherals.pins;
+    let mut led = PinDriver::output(pins.gpio5).unwrap(); // Se asigna el pint 5 a una variable de caracter mutable
 
     loop {
-        pin.set_high().unwrap();
+        led.set_high().unwrap(); // Se enciende el led
         log::info!("LED encendido");
         FreeRtos::delay_ms(500);
 
-        pin.set_low().unwrap();
+        led.set_low().unwrap(); // Se apaga el led
         log::info!("LED apagado");
-        FreeRtos::delay_ms(500);
+        FreeRtos::delay_ms(500); // Delay de 500 ms
     }
 }
